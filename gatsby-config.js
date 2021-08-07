91f5f251d0bc3fafadeff2dd34c5c03c47d51b5a
davidchandra95 <david.chandra@tokopedia.com>
2021-08-07 19:13:03 +0700

    set prefix manually

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     social: siteConfig.author.social,
     disqusShortname: siteConfig.disqusShortname,
   },
-  pathPrefix: siteConfig.pathPrefix, // TODO: Add path prefix as github repository name to deploy to github pages.
+  pathPrefix: '/bloggie', // TODO: Add path prefix as github repository name to deploy to github pages.
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
